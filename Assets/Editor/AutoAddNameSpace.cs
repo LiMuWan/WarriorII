9f75ed1206a43dfe9236b1270ef217f31a6ce8d9
Liruirui <18335929360@163.com>
2019-05-16 10:58:22 +0800

    [李瑞瑞]编写一个创建脚本代码的工具，提升代码编写效率

---
+++ Assets/Editor/AutoAddNameSpace.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+namespace CustomTool
+{
+    public class AutoAddNameSpace : UnityEditor.AssetModificationProcessor
+    {
+        private static void OnWillCreateAsset(string path)
+        {
+            path = path.Replace(".meta", "");
+            if(path.EndsWith(".cs"))
+            {
+                string text = "";
+                //读取xml和json建议用File.ReadAllText，因为它会自动的关闭IO流
+                text += File.ReadAllText(path);
+                Debug.Log( GetClassName1(text));
+                var newText = GetNewScriptContext(GetClassName(text));
+                File.WriteAllText(path, newText);
+            }
+        }
+
+        private static void CreateClass()
+        {
+             
+        }
+
+        private static string GetNewScriptContext(string className)
+        {
+            var script = new ScriptBuildHelp();
+            script.WriteUsing("UnityEngine");
+            script.WriteNameSpace("CustomGame");
+            script.IndentTimes++;
+            script.WriteClass(className);
+            script.IndentTimes++;
+            script.WriteFun("Start");
+
+            return script.ToString();
+        }
+
+        private static string GetClassName(string text)
+        {
+            string[] data = text.Split(' ');
+
+            int index = 0;
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                if(data[i].Contains("class"))
+                {
+                    index = i + 1;
+                    break;
+                }
+            }
+
+            if(data[index].Contains(":"))
+            {
+               return data[index].Split(':')[0];
+            }
+            else
+            {
+                return data[index];
+            }
+        }
+
+        private static string GetClassName1(string text)
+        {
+            //匹配模式 \s代表空格 
+            string patterm = "public class ([A-Za-z0-9_]+)\\s*:\\s*MonoBehaviour";
+            var match = Regex.Match(text, patterm);
+            if(match.Success)
+            {
+                return match.Groups[1].Value;
+            }
+
+            return "";
+        }
+    }
+}
+
+
---
+++ Assets/Editor/ScriptBuildHelp.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace CustomTool
+{
+    public class ScriptBuildHelp
+    {
+        private StringBuilder stringBuilder;
+
+        private string lineBreak = "\r\n";
+
+        private int currentIndex = 0;
+        public int IndentTimes { get; set; }
+        public ScriptBuildHelp()
+        {
+            stringBuilder = new StringBuilder();
+        }
+
+
+        private void Write(string context, bool needIndent = false)
+        {
+            if(needIndent)
+            {
+                context = GetIndent() + context;
+            }
+
+            if(currentIndex == stringBuilder.Length)
+            {
+                stringBuilder.Append(context);
+            }
+            else
+            {
+                stringBuilder.Insert(currentIndex, context);
+            }
+
+            currentIndex += context.Length;
+        }
+
+        private void WriteLine(string context,bool needIndent = false)
+        {
+            Write(context + lineBreak, needIndent);
+        }
+
+        private string GetIndent()
+        {
+            string indent = "";
+            for (int i = 0; i < IndentTimes; i++)
+            {
+                indent += "    "; 
+            }
+            return indent;
+        }
+
+        private int WriteCurlyBrackets()
+        {
+            string start = lineBreak + GetIndent() + "{" + lineBreak;
+            string end = GetIndent() + "}" + lineBreak;
+            Write(start + end, true);
+            return end.Length;
+            
+        }
+
+        public void WriteUsing(string nameSpaceName)
+        {
+            WriteLine("using " + nameSpaceName + ";");
+        }
+
+        public void WriteNameSpace(string name)
+        {
+            Write("namespace " + name);
+            int length = WriteCurlyBrackets();
+            currentIndex -= length;
+        }
+
+        public void WriteClass(string name)
+        {
+            Write("public class " + name + " : MonoBehaviour",true);
+            int length = WriteCurlyBrackets();
+            currentIndex -= length;
+        }
+
+        public void WriteFun(string name,params string[] paraName)
+        {
+            StringBuilder temp = new StringBuilder();
+            temp.Append("public void " + name + "()");
+            if (paraName != null && paraName.Length > 0)
+            {
+                foreach (var s in paraName)
+                {
+                    temp.Insert(temp.Length - 1, s + ",");
+                }
+                temp.Remove(temp.Length - 2, 1);
+            }
+            Write(temp.ToString(), true);
+            WriteCurlyBrackets();
+        }
+
+        public override string ToString()
+        {
+            return stringBuilder.ToString();
+        }
+    }
+}
