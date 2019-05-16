f10d1b58168da13032194ca734e09096e9a21f7b
Liruirui <18335929360@163.com>
2019-05-16 20:07:07 +0800

    [李瑞瑞] 验证已有代码函数功能

---
+++ Assets/Scripts/UI/View/MainMenuView.cs
@@ -0,0 +1,17 @@
+using Const;
+using UnityEngine;
+
+namespace UIFrame
+{
+    public class MainMenuView : BasicUI    
+    {
+        public override UiId GetUiId()
+        {
+            return UiId.MainMenu;
+        }
+
+        public void Start()        
+        {
+        }
+    }
+}
---
+++ Assets/Scripts/UI/View/StartGameView.cs
@@ -0,0 +1,17 @@
+using Const;
+using UnityEngine;
+
+namespace UIFrame
+{
+    public class StartGameView : BasicUI  
+    {
+        public override UiId GetUiId()
+        {
+            return UiId.StartGame;
+        }
+
+        public void Start()        
+        {
+        }
+    }
+}
