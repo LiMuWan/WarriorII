f089c4c714a1e8d5543db11ac67ee18280a9c7b5
李瑞瑞 <18335929360@163.com>
2019-07-18 14:13:28 +0800

    [李瑞瑞]添加SkillItem资源缓存

--- Assets/Scripts/Game/View/HumanSkillView.cs
+++ Assets/Scripts/Game/View/HumanSkillView.cs
@@ -1,12 +1,36 @@
 using Entitas;
 using Entitas.Unity;
+using Manager;
+using System.Collections.Generic;
+using UnityEngine;
+
 namespace Game.View
 {
     public class HumanSkillView:ViewBase     
     {
+        List<GameObject> itemList;
         public  override void Init(Contexts contexts,IEntity entity)        
         {
             base.Init(contexts,entity);
+            itemList = new List<GameObject>();
+        }
+
+        private void SpawItem(string skillCode)
+        {
+            if (itemList.Count < skillCode.Length)
+            {
+                GameObject go = null;
+                foreach (char c in skillCode)
+                {
+                    SpawNewItem(go, c);
+                }
+            }
+        }
+
+        private void SpawNewItem(GameObject go,char code)
+        {
+            go = LoadManager.Single.LoadAndInstantiate(Const.Path.GAME_UI_PATH + code, transform);
+            itemList.Add(go);
         }
     }
 }
