352d5d615daa3f0e994a2d1f1199097754a1f868
李瑞瑞 <18335929360@163.com>
2019-07-18 14:14:01 +0800

    [李瑞瑞]添加HumanSkillSprite组件

---
+++ Assets/Scripts/Game/View/Component/HumanSkillSprite.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanSkillSprite : MonoBehaviour
+{
+    [SerializeField]
+    public Sprite X;
+
+    [SerializeField]
+    public Sprite O;
+}
