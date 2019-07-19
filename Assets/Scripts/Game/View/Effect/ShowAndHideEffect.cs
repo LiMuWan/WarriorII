e48115b6556e7622c615b3b18fbfba3394daeae1
李瑞瑞 <18335929360@163.com>
2019-07-19 17:33:00 +0800

    [李瑞瑞]添加ShowAndHideEffect扩展类

---
+++ Assets/Scripts/Game/View/Effect/ShowAndHideEffect.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+namespace Game.Effect
+{
+    public static class ShowAndHideEffect
+    {
+        public static void ShowImage(this GameObject go, float duration)
+        {
+
+        }
+
+        public static void HideImage(this GameObject go, float duration)
+        {
+
+        }
+    }
+}
