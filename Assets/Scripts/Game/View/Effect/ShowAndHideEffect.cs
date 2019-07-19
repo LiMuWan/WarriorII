7cc46baa8fe86f80fe8da5f8ce957ab3d2392179
李瑞瑞 <18335929360@163.com>
2019-07-19 17:44:30 +0800

    [李瑞瑞]完成Image渐隐渐显效果功能函数

--- Assets/Scripts/Game/View/Effect/ShowAndHideEffect.cs
+++ Assets/Scripts/Game/View/Effect/ShowAndHideEffect.cs
@@ -1,18 +1,42 @@
 ﻿using UnityEngine;
 using DG.Tweening;
+using UnityEngine.UI;
 
 namespace Game.Effect
 {
     public static class ShowAndHideEffect
     {
+        /// <summary>
+        /// 渐显当前物体及子物体所有的Image
+        /// </summary>
+        /// <param name="go"></param>
+        /// <param name="duration"></param>
         public static void ShowImage(this GameObject go, float duration)
         {
-
+            foreach (Image image in go.transform.GetComponentsInChildren<Image>())
+            {
+                KillImageEffect(image);
+                image.DOFade(1, duration);
+            }
         }
 
+        /// <summary>
+        /// 渐隐当前物体及子物体所有的Image
+        /// </summary>
+        /// <param name="go"></param>
+        /// <param name="duration"></param>
         public static void HideImage(this GameObject go, float duration)
         {
+            foreach (Image image in go.transform.GetComponentsInChildren<Image>())
+            {
+                KillImageEffect(image);
+                image.DOFade(0, duration);
+            }
+        }
 
+        private static void KillImageEffect(Image image)
+        {
+            image.DOKill();
         }
     }
 }
