28e28e1842c803f3d031dc03d18e67a9cb47f867
李瑞瑞 <18335929360@163.com>
2019-07-23 11:49:38 +0800

    [李瑞瑞]添加玩家人物技能数据配置及读取配置的逻辑

--- Assets/Scripts/Manager/ModelManager.cs
+++ Assets/Scripts/Manager/ModelManager.cs
@@ -13,9 +13,17 @@ namespace Manager
         /// 玩家数据配置类
         /// </summary>
         public PlayerDataModel PlayerData{ get; private set; }
+
+        /// <summary>
+        /// 玩家技能数据配置类
+        /// </summary>
+        public HumanSkillDataModel HumanSkillDataModel { get; private set; }
+
         public void Init()        
         {
-            PlayerData = ConfigManager.Single.LoadJson<PlayerDataModel>();
+            PlayerData = ConfigManager.Single.LoadJson<PlayerDataModel>(Const.ConfigPath.PLAYER_CONFIG);
+            HumanSkillDataModel = ConfigManager.Single.LoadJson<HumanSkillDataModel>(Const.ConfigPath.HUMAN_SKILL_CONFIG);
+            Debug.Log(HumanSkillDataModel.Skills[0].Code[0]);
         }
     }
 }
---
+++ Assets/Scripts/Model/HumanSkillDataModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System;
+namespace Game
+{
+    [Serializable]
+    public class HumanSkillDataModel 
+    {
+        public List<HumanSkillModel> Skills;
+    }
+
+    [Serializable]
+    public class HumanSkillModel
+    {
+        public string Code;
+        public int Level;
+    }
+}
