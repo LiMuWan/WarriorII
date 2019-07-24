64e48e4bb4335bc7066081b4bc776490e48497f8
李瑞瑞 <18335929360@163.com>
2019-07-24 17:22:08 +0800

    [李瑞瑞]添加自定义事件脚本

--- Assets/Scripts/Game/Function/CustomAniEventManager.cs
+++ Assets/Scripts/Game/Function/CustomAniEventManager.cs
@@ -5,34 +5,72 @@ using System;
 
 namespace Game
 {
+    /// <summary>
+    /// 自定义动画事件管理类
+    /// </summary>
     public class CustomAniEventManager
     {
         private Dictionary<PlayerAniStateName, AnimatorState> stateDic;
+        private Dictionary<PlayerAniStateName, CustomAniEvent> eventDic;
 
         public CustomAniEventManager(Animator animator)
         {
             stateDic = new Dictionary<PlayerAniStateName, AnimatorState>();
+            eventDic = new Dictionary<PlayerAniStateName, CustomAniEvent>();
 
+            InitAnimatorStateData(animator);
+            
+        }
+
+        private void InitAnimatorStateData(Animator animator)
+        {
             AnimatorController aniController = animator.runtimeAnimatorController as AnimatorController;
             AnimatorStateMachine aniMachine = aniController.layers[0].stateMachine;
 
             foreach (ChildAnimatorState state in aniMachine.states)
             {
-                
+
                 foreach (PlayerAniStateName stateName in Enum.GetValues(typeof(PlayerAniStateName)))
                 {
-                    if(state.state.name == stateName.ToString())
+                    if (state.state.name == stateName.ToString())
                     {
                         stateDic[stateName] = state.state;
                     }
                 }
 
-                if(!stateDic.ContainsValue(state.state))
+                if (!stateDic.ContainsValue(state.state))
                 {
                     Debug.LogError("can not find Enum(PlayerAniStateName) name is " + state.state.name);
                 }
             }
-            
+        }
+
+        /// <summary>
+        /// 添加自定义事件脚本
+        /// </summary>
+        private void AddCustomAniEventScripts()
+        {
+            CustomAniEvent behaviourTemp = null;
+            foreach (KeyValuePair<PlayerAniStateName,AnimatorState> pair in stateDic)
+            {
+                foreach (StateMachineBehaviour behaviour in pair.Value.behaviours)
+                {
+                    if(behaviour is CustomAniEvent)
+                    {
+                        behaviourTemp = behaviour as CustomAniEvent;
+                        break;
+                    }
+                }
+
+                if (behaviourTemp == null)
+                {
+                    eventDic[pair.Key] = pair.Value.AddStateMachineBehaviour<CustomAniEvent>();
+                }
+                else
+                {
+                    eventDic[pair.Key] = behaviourTemp;
+                }
+            }
         }
     }
 
