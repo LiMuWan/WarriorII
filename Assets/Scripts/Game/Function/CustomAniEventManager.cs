5d474c0d40c26ba933b777bc3ac916849f3fff54
李瑞瑞 <18335929360@163.com>
2019-07-24 16:35:58 +0800

    [李瑞瑞]缓存动画状态

--- Assets/Scripts/Game/Function/CustomAniEventManager.cs
+++ Assets/Scripts/Game/Function/CustomAniEventManager.cs
@@ -1,19 +1,51 @@
+using System.Collections.Generic;
 using UnityEditor.Animations;
 using UnityEngine;
+using System;
 
 namespace Game
 {
     public class CustomAniEventManager
     {
+        private Dictionary<PlayerAniStateName, AnimatorState> stateDic;
+
         public CustomAniEventManager(Animator animator)
         {
+            stateDic = new Dictionary<PlayerAniStateName, AnimatorState>();
+
             AnimatorController aniController = animator.runtimeAnimatorController as AnimatorController;
             AnimatorStateMachine aniMachine = aniController.layers[0].stateMachine;
 
             foreach (ChildAnimatorState state in aniMachine.states)
             {
-                Debug.Log(state.state.name);
+                
+                foreach (PlayerAniStateName stateName in Enum.GetValues(typeof(PlayerAniStateName)))
+                {
+                    if(state.state.name == stateName.ToString())
+                    {
+                        stateDic[stateName] = state.state;
+                    }
+                }
+
+                if(!stateDic.ContainsValue(state.state))
+                {
+                    Debug.LogError("can not find Enum(PlayerAniStateName) name is " + state.state.name);
+                }
             }
+            
         }
+    }
+
+    /// <summary>
+    /// 动画状态名称对应枚举
+    /// </summary>
+    public enum PlayerAniStateName
+    {
+        idle,
+        walk,
+        run,
+        idleSword,
+        walkSword,
+        runSword
     }
 }
