c16eb19c139f50cdb128d0975bb9f43c3903a951
李瑞瑞 <18335929360@163.com>
2019-07-31 17:29:45 +0800

    [李瑞瑞]添加刀光渐隐渐现特效

---
+++ Assets/Scripts/Game/View/Effect/TrailsEffect.cs
@@ -0,0 +1,32 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace Game.Effect
+{
+    public class TrailsEffect : MonoBehaviour
+    {
+        private float duration;
+        private Material material;
+
+        void Start()
+        {
+            duration = 0.3f;
+            material = transform.GetComponent<MeshRenderer>().material;
+        }
+
+        public void Show()
+        {
+            Effect(1);
+        }
+
+        public void Hide()
+        {
+            Effect(0);
+        }
+
+        private void Effect(float endValue)
+        {
+            material.DOFade(endValue, "_TintColor", duration);
+        }
+    }
+}
