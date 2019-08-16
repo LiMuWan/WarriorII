41488b2721080d400e6ff1dbfb9fa23e774e0888
李瑞瑞 <18335929360@163.com>
2019-08-16 17:35:11 +0800

    [李瑞瑞]编写锁子特效脚本

--- Assets/Scripts/Game/Function/GamePart/Part.cs
+++ Assets/Scripts/Game/Function/GamePart/Part.cs
@@ -7,12 +7,16 @@ namespace Game.GamePart
     {
         private LevelGamePartID levelGamePartID;
         private LevelPartID levelPartId;
+        private ZamekEffect[] zamekEffects;
 
         public void Init(LevelGamePartID levelGamePartID,LevelPartID levelPartId)
         {
             this.levelGamePartID = levelGamePartID;
             this.levelPartId = levelPartId;
-            transform.Find(Const.ConstValue.LEVEL_PART_WALL);
+            Transform wall = transform.Find(Const.ConstValue.LEVEL_PART_WALL);
+            zamekEffects = InitZamek(wall);
+            bool isOpen = JudgeOpenState();
+            SetOpenState(isOpen);
         }
 
         /// <summary>
@@ -23,6 +27,26 @@ namespace Game.GamePart
         {
             return levelGamePartID <= DataManager.Single.LevelGamePartIndex
                 && levelPartId <= DataManager.Single.LevelPartIndex;
+        }
+
+        private ZamekEffect[] InitZamek(Transform wall)
+        {
+            MeshRenderer[] renders = wall.GetComponentsInChildren<MeshRenderer>();
+            ZamekEffect[] zamekEffects = new ZamekEffect[renders.Length];
+            for (int i = 0; i < renders.Length; i++)
+            {
+                zamekEffects[i] = renders[i].gameObject.AddComponent<ZamekEffect>();
+                zamekEffects[i].Init();
+            }
+            return zamekEffects;
+        }
+
+        private void SetOpenState(bool isOpen)
+        {
+            foreach (ZamekEffect effect in zamekEffects)
+            {
+                effect.OpenZamekState(isOpen);
+            }
         }
     }
 }
--- Assets/Scripts/Game/Function/GamePart/ZamekEffect.cs
+++ Assets/Scripts/Game/Function/GamePart/ZamekEffect.cs
@@ -1,22 +1,49 @@
+using DG.Tweening;
 using UnityEngine;
 
 namespace Game.GamePart
 {
     public class ZamekEffect:MonoBehaviour     
     {
-        public void Start()
-        {
+        private Vector3 defaultScale;
+        private float duration;
+        private float punchDuration;
 
+        public void Init()
+        {
+            defaultScale = transform.localScale;
+            duration = 0.7f;
+            punchDuration = 0.3f;
         }
 
-        public void ShowZamek()
+        public void OpenZamekState(bool isOpen)
         {
-
+            if(isOpen)
+            {
+                ShowZamek();
+            }
+            else
+            {
+                HideZamek();
+            }
         }
 
-        public void HideZamek()
+        private void ShowZamek()
         {
+            transform.DOKill();
+            transform.localScale = Vector3.zero;
+            
+            transform.DOScale(defaultScale,duration);
+            transform.DOPunchScale(Vector3.one, punchDuration);
+        }
 
+        private void HideZamek()
+        {
+            transform.DOKill();
+            transform.localScale = defaultScale;
+
+            transform.DOScale(Vector3.zero, duration);
+            transform.DOPunchScale(Vector3.one, punchDuration);
         }
     }
 }
