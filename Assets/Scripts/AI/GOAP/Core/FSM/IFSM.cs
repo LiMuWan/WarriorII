6ff4451981376f68ce98095bbc2ba1df62a5822d
李瑞瑞 <18335929360@163.com>
2019-09-06 11:21:30 +0800

    [李瑞瑞]有限状态机功能设计

--- Assets/Scripts/AI/GOAP/Core/FSM/IFSM.cs
+++ Assets/Scripts/AI/GOAP/Core/FSM/IFSM.cs
@@ -1,11 +1,12 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace GOAP
 {
     public interface IFSM<TLabel>
     {
-       TLabel CurrentState { get; }
-        TLabel BeforeTheState { get; }
+        TLabel CurrentState { get; }
+        TLabel PreviousState { get; }
         void AddState(TLabel label,IFrameState<TLabel> state);
         void ChangeState(TLabel newState);
         void FrameFun();
@@ -17,5 +18,36 @@ namespace GOAP
         void Enter();
         void Excute();
         void Exit();
+    }
+
+    public class FSM<TLabel> : IFSM<TLabel>
+    {
+        public TLabel CurrentState { get { return _currentState.Label; } }
+
+        public TLabel PreviousState { get { return _previousState.Label; } }
+
+        private IFrameState<TLabel> _currentState;
+        private IFrameState<TLabel> _previousState;
+        private Dictionary<TLabel, IFrameState<TLabel>> _stateDic;
+
+        public FSM()
+        {
+            _stateDic = new Dictionary<TLabel, IFrameState<TLabel>>();
+        }
+
+        public void AddState(TLabel label,IFrameState<TLabel> state)
+        {
+            _stateDic.Add(label, state);
+        }
+
+        public void ChangeState(TLabel newState)
+        {
+          
+        }
+
+        public void FrameFun()
+        {
+            _currentState?.Excute();
+        }
     }
 }
