ca85f33dfd861a6e8f46b1184e60d7b0d605bddc
李瑞瑞 <18335929360@163.com>
2019-09-10 13:53:39 +0800

    [李瑞瑞]GoalManager的功能逻辑实现

--- Assets/Scripts/AI/GOAP/Core/Goal/IGoalManager.cs
+++ Assets/Scripts/AI/GOAP/Core/Goal/IGoalManager.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace GOAP
@@ -14,6 +16,86 @@ namespace GOAP
 
         IGoal<TGoal> FindGoal();
 
-        void UpdateData();
+        void UpdateData(); 
+    }
+
+    public abstract class GoalManager<TAction,TGoal> : IGoalManager<TGoal>
+    {
+        public IGoal<TGoal> Current { get; private set; }
+
+        private Dictionary<TGoal, IGoal<TGoal>> _goalsDic;
+        private List<IGoal<TGoal>> _activeGoals;
+ 
+        private IAgent<TAction, TGoal> _agent;
+
+        public GoalManager(IAgent<TAction, TGoal> agent)
+        {
+            _agent = agent;
+            _goalsDic = new Dictionary<TGoal, IGoal<TGoal>>();
+            _activeGoals = new List<IGoal<TGoal>>();
+            InitGoals();
+            
+        }
+
+        protected abstract void InitGoals();
+
+        public void AddGoal(TGoal label)
+        {
+            var goal = _agent.Map.GetGoal(label);
+            if(goal != null)
+            {
+                _goalsDic.Add(label, goal);
+
+                goal.AddGoalActivateListener((currentGoal) =>
+                {
+                    if(!_activeGoals.Contains(currentGoal))
+                    {
+                        _activeGoals.Add(currentGoal);
+                    }
+                });
+
+                goal.AddGoalInactivateListener((currentGoal) =>
+                {
+                    if(_activeGoals.Contains(currentGoal))
+                    {
+                        _activeGoals.Remove(currentGoal);
+                    }
+                });
+            }
+        }
+
+        public void RemoveGoal(TGoal label)
+        {
+            _goalsDic.Remove(label);
+        }
+
+        public IGoal<TGoal> GetGoal(TGoal label)
+        {
+            if(_goalsDic.ContainsKey(label))
+            {
+                return _goalsDic[label];
+            }
+
+            DebugMsg.LogError("当前代理未初始化此目标，标签为 ：" + label);
+            return null;
+        }
+
+        public IGoal<TGoal> FindGoal()
+        {
+            _activeGoals = _activeGoals.OrderByDescending(u => u.GetPriority()).ToList();
+            if(_activeGoals.Count > 0)
+            {
+                return _activeGoals[0];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public void UpdateData()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
