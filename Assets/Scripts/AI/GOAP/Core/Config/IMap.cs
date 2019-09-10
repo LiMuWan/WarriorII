8c95fa7b984d2a5b3387759336575d0cfc33ecc7
李瑞瑞 <18335929360@163.com>
2019-09-10 11:00:45 +0800

    [李瑞瑞]Map中补充Goal的映射逻辑

--- Assets/Scripts/AI/GOAP/Core/Config/IMap.cs
+++ Assets/Scripts/AI/GOAP/Core/Config/IMap.cs
@@ -6,16 +6,21 @@ namespace GOAP
     public interface IMap<TAction,TGoal>
     {
         IActionHandler<TAction> GetActionHandler(TAction actionLabel);
+
+        IGoal<TGoal> GetGoal(TGoal goalLabel);
     }
 
     public abstract class MapBase<TAction,TGoal> : IMap<TAction, TGoal>
     {
         private Dictionary<TAction, IActionHandler<TAction>> _actionHandlerDic;
+        private Dictionary<TGoal, IGoal<TGoal>> _goalDic;
 
         public MapBase()
         {
             _actionHandlerDic = new Dictionary<TAction, IActionHandler<TAction>>();
+            _goalDic = new Dictionary<TGoal, IGoal<TGoal>>();
             InitActionMap();
+            InitGoalMap();
         }
 
         public IActionHandler<TAction> GetActionHandler(TAction actionLabel)
@@ -29,7 +34,20 @@ namespace GOAP
             return handler;
         }
 
+        public IGoal<TGoal> GetGoal(TGoal goalLabel)
+        {
+            IGoal<TGoal> goal = null;
+            _goalDic.TryGetValue(goalLabel, out goal);
+
+            if (goal == null)
+                DebugMsg.LogError("当前映射中未找到对应IGoal,标签名称为： " + goalLabel);
+
+            return goal;
+        }
+
         protected abstract void InitActionMap();
+
+        protected abstract void InitGoalMap();
 
         protected void AddAction(IActionHandler<TAction> handler)
         {
@@ -40,6 +58,18 @@ namespace GOAP
             else
             {
                 DebugMsg.LogError("发现具有重复标签的Handler，标签为：" + handler.Label);
+            }
+        }
+
+        protected void AddGoal(IGoal<TGoal> goal)
+        {
+            if(!_goalDic.ContainsKey(goal.Lable))
+            {
+                _goalDic.Add(goal.Lable,goal);
+            }
+            else
+            {
+                DebugMsg.LogError("发现具有重复标签的Goal,标签为 ： " + goal.Lable);
             }
         }
     }
