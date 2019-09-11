2917d56cce65f7bc903e16e666054b652f4fbfaf
李瑞瑞 <18335929360@163.com>
2019-09-11 10:20:19 +0800

    [李瑞瑞]自动id的实现

--- Assets/Scripts/AI/GOAP/Core/Tree/Tree.cs
+++ Assets/Scripts/AI/GOAP/Core/Tree/Tree.cs
@@ -9,6 +9,21 @@ namespace GOAP
 
     public class TreeNode<TAction>
     {
+        public  static int _id ;
 
+        public int ID { get; private set; }
+       
+        public IActionHandler<TAction> ActionHandler { get; private set; }
+
+        public TreeNode(IActionHandler<TAction> actionHandler)
+        {
+            ID = _id++;
+            ActionHandler = actionHandler;
+        }
+
+        public void Reset()
+        {
+            _id = 0;
+        }
     }
 }
