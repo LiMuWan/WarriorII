8a1c61709467f86d202b9e02cd07fe630a64abdd
李瑞瑞 <18335929360@163.com>
2019-09-17 14:03:33 +0800

    [李瑞瑞]执行计划的功能编写

--- Assets/Scripts/AI/GOAP/Core/Planner/IPlannerHandler.cs
+++ Assets/Scripts/AI/GOAP/Core/Planner/IPlannerHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace GOAP
@@ -6,34 +7,78 @@ namespace GOAP
     public interface IPlannerHandler<TAction>
     {
         bool IsComplete { get; }
-        void Init(Queue<IActionHandler<TAction>> plan);
+        void Init(IActionManager<TAction> actionManager, Queue<IActionHandler<TAction>> plan);
         void StartPlan();
         void NextAction();
         void Interruptible();
+        void AddCompleteCallBack(Action onComplete);
     }
 
     public class PlannerHandler<TAction> : IPlannerHandler<TAction>
     {
-        public bool IsComplete { get; private set; }
-
-        public void Init(Queue<IActionHandler<TAction>> plan)
+        public bool IsComplete
         {
-            throw new System.NotImplementedException();
+            get
+            {
+                if (_isInterruptible)
+                    return true;
+
+                if (_plan == null)
+                    return true;
+
+                if(_currentActionHandler == null)
+                {
+                    return _plan.Count == 0;
+                }
+                else
+                {
+                    return _currentActionHandler.IsComplete && _plan.Count == 0;
+                }
+            }
+        }
+
+        private Action _onComplete;
+
+        private Queue<IActionHandler<TAction>> _plan;
+        private IActionHandler<TAction> _currentActionHandler;
+        private IActionManager<TAction> _actionManager;
+        private bool _isInterruptible;
+
+        public void Init(IActionManager<TAction> actionManager,Queue<IActionHandler<TAction>> plan)
+        {
+            _plan = plan;
+            _actionManager = actionManager;
+            _currentActionHandler = null;
+            _onComplete = null;
+            _isInterruptible = false;
         }
 
         public void StartPlan()
         {
-            throw new System.NotImplementedException();
+            NextAction();
         }
 
         public void NextAction()
         {
-            throw new System.NotImplementedException();
+           if(IsComplete)
+            {
+                _onComplete?.Invoke();
+            }
+           else
+            {
+                _currentActionHandler = _plan.Dequeue();
+                _actionManager.ChangeCurrentAction(_currentActionHandler.Label);
+            }
         }
 
         public void Interruptible()
         {
-            throw new System.NotImplementedException();
+            _isInterruptible = true;
+        }
+
+        public void AddCompleteCallBack(Action onComplete)
+        {
+            _onComplete = onComplete;
         }
     }
 }
