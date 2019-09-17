d4bf2c64322e58844d4eddb43702162b7f247a50
李瑞瑞 <18335929360@163.com>
2019-09-17 16:05:54 +0800

    [李瑞瑞]执行部分的初始化

---
+++ Assets/Scripts/AI/GOAP/Core/Perform/IPerform.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace GOAP
+{
+    public interface IPerform
+    {
+        void UpdateData();
+    }
+
+    public class Performer<TAction, TGoal> : IPerform
+    {
+        private IPlannerHandler<TAction> _plannerHandler;
+        private IPlanner<TAction, TGoal> _planner;
+        private IGoalManager<TGoal> _goalManager;
+        private IActionManager<TAction> _actionManager;
+
+        public Performer(IAgent<TAction,TGoal> agent)
+        {
+            _plannerHandler = new PlannerHandler<TAction>();
+            _plannerHandler.AddCompleteCallBack(PlanComplete);
+
+            _planner = new Planner<TAction, TGoal>(agent);
+            _goalManager = agent.GoalManager;
+            _actionManager = agent.ActionManager;
+        }
+
+        private void PlanComplete()
+        {
+
+        }
+
+        public void UpdateData()
+        {
+
+        }
+    }
+}
