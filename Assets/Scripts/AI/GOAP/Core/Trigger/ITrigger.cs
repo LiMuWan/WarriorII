ab95046f1fc99b7957ca176c89a53697666468a5
李瑞瑞 <18335929360@163.com>
2019-09-17 17:26:57 +0800

    [李瑞瑞]触发器设计

---
+++ Assets/Scripts/AI/GOAP/Core/Trigger/ITrigger.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace GOAP
+{
+    public interface ITrigger
+    {
+       bool IsTrigger { get; set; }
+       void FrameFun();
+    }
+
+    public abstract class TriggerBase : ITrigger
+    {
+        public bool IsTrigger { get; set; }
+
+        public void FrameFun()
+        {
+            
+        }
+    }
+}
