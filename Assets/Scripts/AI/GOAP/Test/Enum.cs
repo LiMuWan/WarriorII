0aca59c445e7b832b72a1d747d5c16b5cd81d0b6
李瑞瑞 <18335929360@163.com>
2019-09-18 14:21:32 +0800

    [李瑞瑞]枚举设计

--- Assets/Scripts/AI/GOAP/Test/Enum.cs
+++ Assets/Scripts/AI/GOAP/Test/Enum.cs
@@ -4,16 +4,32 @@ namespace GOAPTest
 {
    public enum ActionEnum
    {
-
+        IDLE,
+        ALERT,
+        MOVE,
+        ATTACK,
+        INJURE,
+        ATTACK_IDLE
    }
 
     public enum GoalEnum
     {
-
+        ATTACK,
+        ALERT,
+        ATTACK_IDLE,
+        INJURE
     }
 
     public enum KeyNameEnum
     {
-
+        IDLE,
+        ALERT,
+        MOVE,
+        ATTACK,
+        INJURE,
+        ATTACK_IDLE,
+        FIND_ENEMY,
+        NEAR_ENEMY,
+        KILLED_ENEMY
     }
 }
