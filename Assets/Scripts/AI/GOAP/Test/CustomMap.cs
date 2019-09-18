9535fa527958430e731018b5d855bb47537fd715
李瑞瑞 <18335929360@163.com>
2019-09-18 15:02:26 +0800

    [李瑞瑞]初始化Idle动作的数据

---
+++ Assets/Scripts/AI/GOAP/Test/CustomMap.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using GOAP;
+
+namespace GOAPTest
+{
+    public class CustomMap : MapBase<ActionEnum, GoalEnum>
+    {
+        public CustomMap() : base() { }
+
+        protected override void InitActionMap()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void InitGameData()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void InitGoalMap()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Assets/Scripts/AI/GOAP/Test/IdleAction.cs
@@ -0,0 +1,31 @@
+﻿using GOAP;
+using UnityEngine;
+
+namespace GOAPTest
+{
+    public class IdleAction : ActionBase<ActionEnum, GoalEnum>
+    {
+        public override ActionEnum Label { get { return ActionEnum.IDLE; } }
+
+        public override int Cost { get { return 1; } }
+
+        public override int Priority { get { return 0; } }
+
+        public override bool CanInterruptiblePlan { get { return false; } }
+
+        public IdleAction(IAgent<ActionEnum,GoalEnum> agent):base(agent)
+        {
+
+        }
+
+        protected override IState InitEffects()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override IState InitPreconditions()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
