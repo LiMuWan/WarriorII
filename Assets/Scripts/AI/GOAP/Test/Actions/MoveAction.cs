7a75ce9dd4a90aa8e4a8f82f566bcd938353c436
李瑞瑞 <18335929360@163.com>
2019-09-18 16:05:28 +0800

    [李瑞瑞]移动动作

---
+++ Assets/Scripts/AI/GOAP/Test/Actions/MoveAction.cs
@@ -0,0 +1,35 @@
+﻿using GOAP;
+using UnityEngine;
+
+namespace GOAPTest
+{
+    public class MoveAction : ActionBase<ActionEnum, GoalEnum>
+    {
+        public override ActionEnum Label { get { return ActionEnum.MOVE; } }
+
+        public override int Cost { get { return 5; } }
+
+        public override int Priority { get { return 70; } }
+
+        public override bool CanInterruptiblePlan { get; }
+
+        public MoveAction(IAgent<ActionEnum, GoalEnum> agent) : base(agent)
+        {
+        }
+
+        protected override IState InitPreconditions()
+        {
+            State<KeyNameEnum> state = new State<KeyNameEnum>();
+            state.Set(KeyNameEnum.FIND_ENEMY, true);
+            return state;
+        }
+
+        protected override IState InitEffects()
+        {
+            State<KeyNameEnum> state = new State<KeyNameEnum>();
+            state.Set(KeyNameEnum.NEAR_ENEMY, true);
+            return state;
+        }
+
+    }
+}
