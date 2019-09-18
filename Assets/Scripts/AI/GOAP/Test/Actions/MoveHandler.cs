937ba3c12454cd4f4ffb6562c75f7af07d5a130e
李瑞瑞 <18335929360@163.com>
2019-09-18 17:26:32 +0800

    [李瑞瑞]完成MoveHandler

--- Assets/Scripts/AI/GOAP/Test/Actions/MoveHandler.cs
+++ Assets/Scripts/AI/GOAP/Test/Actions/MoveHandler.cs
@@ -6,6 +6,8 @@ namespace GOAPTest
     public class MoveHandler : ActionHandlerBase<ActionEnum, GoalEnum>
     {
         private Transform _self, _enemy;
+        private CharacterController _controller;
+        private float _speed = 4;
 
         public MoveHandler(IAgent<ActionEnum, GoalEnum> agent, IAction<ActionEnum> action) : base(agent, action)
         {
@@ -18,6 +20,17 @@ namespace GOAPTest
             DebugMsg.Log("进入移动状态");
             _self = _agent.Map.GetGameData(DataName.SELF_TRANS) as Transform;
             _enemy = _agent.Map.GetGameData(DataName.ENEMY_TRANS) as Transform;
+            _controller = _self.GetComponent<CharacterController>();
+        }
+
+        public override void Excute()
+        {
+            base.Excute();
+            if(Vector3.Distance(_self.position,_enemy.position) > 1.5f)
+            {
+                Vector3 dirToEnemy = (_enemy.position - _self.position).normalized;
+                _controller.SimpleMove(dirToEnemy * _speed);
+            }
         }
     }
 }
