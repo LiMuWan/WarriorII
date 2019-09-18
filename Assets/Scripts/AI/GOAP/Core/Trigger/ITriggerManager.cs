65e1bec1aeb4291b7e27ca25f4673fdf114b51a4
李瑞瑞 <18335929360@163.com>
2019-09-18 09:29:39 +0800

    [李瑞瑞]实现触发器管理类

---
+++ Assets/Scripts/AI/GOAP/Core/Trigger/ITriggerManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace GOAP
+{
+    public interface ITriggerManager
+    {
+        void FrameFun();
+    }
+
+    public abstract class TriggerManagerBase<TAction,TGoal> : ITriggerManager
+    {
+        private HashSet<ITrigger> _triggers;
+
+        public TriggerManagerBase()
+        {
+            _triggers = new HashSet<ITrigger>();
+            InitTriggers();
+        }
+
+        public abstract void InitTriggers();
+
+        public void FrameFun()
+        {
+            
+        }
+    }
+}
