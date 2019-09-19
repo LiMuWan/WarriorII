8afe748f93d4c89d47e04777a8177d6f48abe522
李瑞瑞 <18335929360@163.com>
2019-09-19 09:28:39 +0800

    [李瑞瑞]完成受伤动作状态

---
+++ Assets/Scripts/AI/GOAP/Test/Actions/InjureHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using GOAP;
+using UnityEngine;
+
+namespace GOAPTest
+{
+    public class InjureHandler : ActionHandlerBase<ActionEnum, GoalEnum>
+    {
+        public InjureHandler(IAgent<ActionEnum, GoalEnum> agent, IAction<ActionEnum> action) : base(agent, action)
+        {
+        }
+
+        public async override void Enter()
+        {
+            base.Enter();
+            DebugMsg.Log("进入受伤状态");
+            await Task.Delay(TimeSpan.FromSeconds(2));
+            OnComplete();
+        }
+    }
+}
