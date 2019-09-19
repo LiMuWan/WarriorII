178f71d5d90d362a0f3be1a0b379aeb73de3dfe8
李瑞瑞 <18335929360@163.com>
2019-09-19 09:59:22 +0800

    [李瑞瑞]动作部分初始化

---
+++ Assets/Scripts/AI/GOAP/Test/CurrentActMgr.cs
@@ -0,0 +1,26 @@
+﻿using GOAP;
+using UnityEngine;
+
+namespace GOAPTest
+{
+    public class CurrentActMgr : ActionManagerBase<ActionEnum, GoalEnum>
+    {
+        public CurrentActMgr(IAgent<ActionEnum, GoalEnum> agent) : base(agent)
+        {
+        }
+
+        public override ActionEnum GetDefaultActionLable()
+        {
+            return ActionEnum.IDLE;
+        }
+
+        protected override void InitActionHandler()
+        {
+            AddHandler(ActionEnum.IDLE);
+            AddHandler(ActionEnum.MOVE);
+            AddHandler(ActionEnum.ATTACK);
+            AddHandler(ActionEnum.ATTACK_IDLE);
+            AddHandler(ActionEnum.INJURE);
+        }
+    }
+}
