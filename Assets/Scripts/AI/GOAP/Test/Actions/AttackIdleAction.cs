f59e52374790797bbbd0288d18138d2e98931049
李瑞瑞 <18335929360@163.com>
2019-09-19 09:28:20 +0800

    [李瑞瑞]完成攻击待机动作状态

---
+++ Assets/Scripts/AI/GOAP/Test/Actions/AttackIdleAction.cs
@@ -0,0 +1,36 @@
+﻿using GOAP;
+using UnityEngine;
+
+namespace GOAPTest
+{
+    public class AttackIdleAction : ActionBase<ActionEnum, GoalEnum>
+    {
+        public override ActionEnum Label { get { return ActionEnum.ATTACK_IDLE; } }
+
+        public override int Cost { get { return 1; } }
+
+        public override int Priority { get { return 1; } }
+
+        public override bool CanInterruptiblePlan { get { return false; } }
+
+        public AttackIdleAction(IAgent<ActionEnum, GoalEnum> agent) : base(agent)
+        {
+
+        }
+
+        protected override IState InitPreconditions()
+        {
+            State<KeyNameEnum> state = new State<KeyNameEnum>();
+            state.Set(KeyNameEnum.ATTACK_IDLE, true);
+            return state;
+        }
+
+        protected override IState InitEffects()
+        {
+            State<KeyNameEnum> state = new State<KeyNameEnum>();
+            state.Set(KeyNameEnum.MOVE, true);
+            return state;
+        }
+
+    }
+}
