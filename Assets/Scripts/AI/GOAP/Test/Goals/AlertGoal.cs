4c1ecf8cd8b0945e62fb06a24d0efd26c86d0420
李瑞瑞 <18335929360@163.com>
2019-09-19 14:24:49 +0800

    [李瑞瑞]警戒目标组件

---
+++ Assets/Scripts/AI/GOAP/Test/Goals/AlertGoal.cs
@@ -0,0 +1,36 @@
+﻿using GOAP;
+using UnityEngine;
+
+namespace GOAPTest
+{
+    public class AlertGoal : GoalBase<ActionEnum,GoalEnum>
+    {
+        public AlertGoal(IAgent<ActionEnum, GoalEnum> agent) : base(agent)
+        {
+        }
+
+        public override GoalEnum Lable { get { return GoalEnum.ALERT; } }
+
+        public override float GetPriority()
+        {
+            return 1;
+        }
+
+        public override IState GetEffects()
+        {
+            State<KeyNameEnum> state = new State<KeyNameEnum>();
+            state.Set(KeyNameEnum.MOVE, true);
+            return state;
+        }
+
+        public override bool ActiveCondition()
+        {
+           
+        }
+
+        public override IState InitEffects()
+        {
+            
+        }
+    }
+}
