4acc5a57069e76abdc0c779a89c67e3c2f1c341c
李瑞瑞 <18335929360@163.com>
2019-09-20 10:11:03 +0800

    [李瑞瑞]添加CustomGoalMgr，并完成其初始化配置

---
+++ Assets/Scripts/AI/GOAP/Test/CustomGoalMgr.cs
@@ -0,0 +1,21 @@
+﻿using GOAP;
+using UnityEngine;
+
+namespace GOAPTest
+{
+    public class CustomGoalMgr : GoalManagerBase<ActionEnum, GoalEnum>
+    {
+        public CustomGoalMgr(IAgent<ActionEnum, GoalEnum> agent) : base(agent)
+        {
+
+        }
+
+        protected override void InitGoals()
+        {
+            AddGoal(GoalEnum.ALERT);
+            AddGoal(GoalEnum.ATTACK);
+            AddGoal(GoalEnum.ATTACK_IDLE);
+            AddGoal(GoalEnum.INJURE);
+        }
+    }
+}
