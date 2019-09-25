50674bdc76e8ee1481f135e38b01aec5e4da3c81
李瑞瑞 <18335929360@163.com>
2019-09-25 17:33:40 +0800

    [李瑞瑞]完成攻击动作和攻击状态的逻辑判定

---
+++ Assets/Scripts/AI/Peasant/ActionHandlers/AttackHandler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using BlueGOAP;
+
+namespace Game.AI
+{
+    public class AttackHandler : ActionHandlerBase<ActionEnum, GoalEnum>
+    {
+        public AttackHandler(IAgent<ActionEnum, GoalEnum> agent, IMaps<ActionEnum, GoalEnum> maps, IAction<ActionEnum> action) : base(agent, maps, action)
+        {
+
+        }
+
+        public override void Enter()
+        {
+            base.Enter();
+            DebugMsg.Log("进入攻击状态");
+            //todo: 执行动画，播放声音
+            OnComplete();
+        }
+    }
+}
