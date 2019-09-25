2cd14de5a3d8eb8ccac43db37f703e59333bf9db
李瑞瑞 <18335929360@163.com>
2019-09-25 17:32:30 +0800

    [李瑞瑞]完成攻击待机的动作和攻击待机状态的逻辑判定

---
+++ Assets/Scripts/AI/Peasant/ActionHandlers/IdleSwordHandler.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using BlueGOAP;
+using Game.Service;
+using Module.Timer;
+
+namespace Game.AI
+{
+    public class IdleSwordHandler : ActionHandlerBase<ActionEnum, GoalEnum>
+    {
+        ITimerService _timerService;
+        ITimer _timer;
+
+        public IdleSwordHandler(IAgent<ActionEnum, GoalEnum> agent, IMaps<ActionEnum, GoalEnum> maps, IAction<ActionEnum> action) : base(agent, maps, action)
+        {
+            _timerService = Contexts.sharedInstance.service.gameServiceTimerService.TimerService;
+        }
+
+        public override void Enter()
+        {
+            base.Enter();
+            DebugMsg.Log("进入攻击待机状态");
+            _timer = _timerService.CreateOrRestartTimer("IdleSwordHandler", Const.IDLE_SWORD_DELAY_TIME, false);
+            _timer.AddCompleteListener(() => OnComplete());
+        }
+
+        public override void Exit()
+        {
+            base.Exit();
+            _timerService.StopTimer(_timer, false);
+        }
+    }
+}
