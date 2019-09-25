100d74b1edb6a1133d1cc78a75b62e2ed42c862b
李瑞瑞 <18335929360@163.com>
2019-09-25 17:31:03 +0800

    [李瑞瑞]完成待机动作和待机状态的判定逻辑

---
+++ Assets/Scripts/AI/Peasant/ActionHandlers/IdleHandler.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using BlueGOAP;
+
+namespace Game.AI
+{
+    public class IdleHandler : ActionHandlerBase<ActionEnum, GoalEnum>
+    {
+        public IdleHandler(IAgent<ActionEnum, GoalEnum> agent, IMaps<ActionEnum, GoalEnum> maps, IAction<ActionEnum> action) : base(agent, maps, action)
+        {
+
+        }
+
+        public override void Enter()
+        {
+            base.Enter();
+            DebugMsg.Log("进入待机状态");
+        }
+
+        public override void Execute()
+        {
+            base.Execute();
+
+            if(_agent.AgentState.Get(StateKeyEnum.FIND_ENEMY.ToString()) == true)
+            {
+                OnComplete();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/AI/Peasant/Actions/IdleAction.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using BlueGOAP;
+
+namespace Game.AI
+{
+    public class IdleAction : ActionBase<ActionEnum, GoalEnum>
+    {
+        public override ActionEnum Label { get { return ActionEnum.IDLE; } }
+
+        public override int Cost { get { return 1; } }
+
+        public override int Priority { get { return 1; } }
+
+        public override bool CanInterruptiblePlan { get { return false; } }
+
+        public IdleAction(IAgent<ActionEnum, GoalEnum> agent) : base(agent)
+        {
+
+        }
+
+        protected override IState InitPreconditions()
+        {
+            State<StateKeyEnum> state = new State<StateKeyEnum>();
+            state.Set(StateKeyEnum.ALERT, false);
+            state.Set(StateKeyEnum.FIND_ENEMY, false);
+            state.Set(StateKeyEnum.ATTACK, false);
+            return state;
+        }
+
+        protected override IState InitEffects()
+        {
+            return null;
+        }
+    }
+}
