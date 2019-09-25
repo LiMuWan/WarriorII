a71276754ae60e114e9795bd59ff1ec5e1ec536b
李瑞瑞 <18335929360@163.com>
2019-09-25 17:34:09 +0800

    [李瑞瑞]完成死亡动作和死亡状态逻辑的判定

---
+++ Assets/Scripts/AI/Peasant/ActionHandlers/DeadHandler.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using BlueGOAP;
+
+namespace Game.AI
+{
+    public class DeadHandler : ActionHandlerBase<ActionEnum, GoalEnum>
+    {
+        public DeadHandler(IAgent<ActionEnum, GoalEnum> agent, IMaps<ActionEnum, GoalEnum> maps, IAction<ActionEnum> action) : base(agent, maps, action)
+        {
+
+        }
+
+        public override void Enter()
+        {
+            base.Enter();
+            int injureValue = (int)GetGameData(GameDataKeyEnum.INJURE_VALUE);
+
+            switch (injureValue)
+            {
+                case Const.INSTANT_KILL:
+                    //todo: 一击必杀效果
+                    break;
+                default:
+                    //todo: 正常死亡逻辑
+                    break;
+            }
+
+            DebugMsg.Log("进入死亡状态");
+        }
+    }
+}
