f355220b267ab9b470743c89d4a5b8e5723d7b49
李瑞瑞 <18335929360@163.com>
2019-09-25 17:30:11 +0800

    [李瑞瑞]定义动作枚举和目标枚举、状态枚举键值，定义一些动作中用到的常量

---
+++ Assets/Scripts/AI/AIEnums.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+
+namespace Game.AI
+{
+    /// <summary>
+    /// 动作
+    /// </summary>
+    public enum ActionEnum
+    {
+        IDLE,
+        IDLE_SWORD,
+        ATTACK,
+        INJURE,
+        DEAD,
+        MOVE,
+        MoveBackward
+    }
+
+    /// <summary>
+    /// 目标
+    /// </summary>
+    public enum GoalEnum
+    {
+        IDLE_SWORD,
+        ATTACK,
+        INJURE,
+        DEAD
+    }
+
+    /// <summary>
+    /// 状态键值
+    /// </summary>
+    public enum StateKeyEnum
+    {
+        ALERT,
+        FIND_ENEMY,
+        NEAR_ENEMY,
+        ATTACK,
+        INJURE,
+        DEAD,
+        MOVE,
+        /// <summary>
+        /// 后退
+        /// </summary>
+        STEP_BACK
+    }
+
+    /// <summary>
+    /// 游戏数据键值
+    /// </summary>
+    public enum GameDataKeyEnum
+    {
+        /// <summary>
+        /// 敌方单位对象
+        /// </summary>
+        ENEMY_TRANS,
+        /// <summary>
+        /// 自身对象
+        /// </summary>
+        SELF_TRANS,
+        /// <summary>
+        /// 受伤值
+        /// </summary>
+        INJURE_VALUE,
+        /// <summary>
+        /// 生命值
+        /// </summary>
+        LIFE
+    }
+}
---
+++ Assets/Scripts/AI/Const.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Game.AI
+{
+    public class Const     
+    {
+        /// <summary>
+        /// 攻击待机的延时时间
+        /// </summary>
+        public const float IDLE_SWORD_DELAY_TIME = 2f;
+
+        /// <summary>
+        /// 接近敌人的可攻击距离
+        /// </summary>
+        public const float NEAR_ENEMY_DISTANCE = 1.5f;
+
+        /// <summary>
+        /// 安全距离
+        /// </summary>
+        public const float SAFE_DISTANCE = 5;
+
+        /// <summary>
+        /// 移动速度
+        /// </summary>
+        public const float MOVE_VELOCITY = 1.5f;
+
+        /// <summary>
+        /// 一击必杀伤害数值
+        /// </summary>
+        public const int INSTANT_KILL = 1000;
+    }
+}
