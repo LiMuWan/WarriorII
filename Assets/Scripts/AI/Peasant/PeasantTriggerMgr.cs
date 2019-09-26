b57f121c23a66f136df12b56998727ea1a6010a2
李瑞瑞 <18335929360@163.com>
2019-09-26 12:00:40 +0800

    [李瑞瑞]添加EyesTrigger和完成Peasnt触发器管理器的初始化Trigger

---
+++ Assets/Scripts/AI/Peasant/PeasantTriggerMgr.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using BlueGOAP;
+
+namespace Game.AI
+{
+    public class PeasantTriggerMgr : TriggerManagerBase<ActionEnum, GoalEnum>
+    {
+        public PeasantTriggerMgr(IAgent<ActionEnum, GoalEnum> agent) : base(agent)
+        {
+
+        }
+
+        protected override void InitTriggers()
+        {
+            AddTrigger(new EyesTrigger(_agent));
+        }
+    }
+}
---
+++ Assets/Scripts/AI/Peasant/Triggers/EyesTrigger.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using BlueGOAP;
+
+namespace Game.AI
+{
+    public class EyesTrigger : TriggerBase<ActionEnum, GoalEnum>
+    {
+        public override int Priority { get { return 1; } }
+        public override bool IsTrigger {
+            get
+            {
+                if(_enemy == null ||  _self == null)
+                    return false;
+                //比对发现目标的距离
+                if(Vector3.Distance(_self.position,_enemy.position) < Const.FIND_ENEMY_DISTANCE)
+                {
+                    //查看是否再视线角度内
+                    Vector3 dirToEnemy = (_enemy.position - _self.position).normalized;
+
+                    if(Vector3.Angle(_self.forward,dirToEnemy) < Const.SIGHT_LINE_RANGE)
+                    {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+            set { }
+        }
+
+        private Transform _self, _enemy;
+
+        public EyesTrigger(IAgent<ActionEnum, GoalEnum> agent) : base(agent)
+        {
+            _self = agent.Maps.GetGameData(GameDataKeyEnum.SELF_TRANS) as Transform;
+            _enemy = agent.Maps.GetGameData(GameDataKeyEnum.ENEMY_TRANS) as Transform;
+        }
+
+        protected override IState InitEffects()
+        {
+            State<StateKeyEnum> state = new State<StateKeyEnum>();
+            state.Set(StateKeyEnum.FIND_ENEMY, true);
+            return state;
+        }
+    }
+}
