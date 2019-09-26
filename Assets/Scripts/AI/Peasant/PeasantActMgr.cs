5a546dae49dbce649b02a81c4c01fd8fab7cd01a
李瑞瑞 <18335929360@163.com>
2019-09-26 11:55:08 +0800

    [李瑞瑞]完成Peasant动作管理器

---
+++ Assets/Scripts/AI/Peasant/PeasantActMgr.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using BlueGOAP;
+
+namespace Game.AI
+{
+    public class PeasantActMgr : ActionManagerBase<ActionEnum, GoalEnum>
+    {
+        public PeasantActMgr(IAgent<ActionEnum, GoalEnum> agent) : base(agent)
+        {
+
+        }
+
+        public override ActionEnum GetDefaultActionLabel()
+        {
+            return ActionEnum.IDLE;
+        }
+
+        protected override void InitActionHandlers()
+        {
+            AddActionHandler(ActionEnum.IDLE);
+            AddActionHandler(ActionEnum.ATTACK);
+            AddActionHandler(ActionEnum.MOVE);
+        }
+
+        protected override void InitActionStateHandlers()
+        {
+            AddActionStateHandler(ActionEnum.ALERT);
+        }
+    }
+}
