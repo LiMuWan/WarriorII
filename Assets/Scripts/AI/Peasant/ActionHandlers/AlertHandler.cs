fb2310e1f9dba4d07772a1963e7cd65640ad4e95
李瑞瑞 <18335929360@163.com>
2019-09-26 11:51:34 +0800

    [李瑞瑞] 添加警戒动作和警戒状态执行逻辑

---
+++ Assets/Scripts/AI/Peasant/ActionHandlers/AlertHandler.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using BlueGOAP;
+
+namespace Game.AI
+{
+    public class AlertHandler : ActionHandlerBase<ActionEnum, GoalEnum>
+    {
+        private Transform _self, _enemy;
+
+        public AlertHandler(IAgent<ActionEnum, GoalEnum> agent, IMaps<ActionEnum, GoalEnum> maps, IAction<ActionEnum> action) : base(agent, maps, action)
+        {
+
+        }
+
+        public override void Enter()
+        {
+            base.Enter();
+            DebugMsg.Log("进入警戒状态");
+            _self = GetGameData(GameDataKeyEnum.SELF_TRANS) as Transform;
+            _enemy = GetGameData(GameDataKeyEnum.ENEMY_TRANS) as Transform;
+        }
+
+        public override void Execute()
+        {
+            base.Execute();
+            int life = (int)GetGameData(GameDataKeyEnum.LIFE);
+            if(life > 0 && _agent.AgentState.ContainState(Action.Preconditions))
+            {
+                _self.LookAt(_enemy);
+            }
+            else
+            {
+                OnComplete();
+            }
+        }
+    }
+}
