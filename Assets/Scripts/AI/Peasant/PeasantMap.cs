ffc065cc2eb1a99c7b1908165acb49f99800d072
李瑞瑞 <18335929360@163.com>
2019-09-26 11:59:30 +0800

    [李瑞瑞]完成Peasant的初始化动作、目标配置

---
+++ Assets/Scripts/AI/Peasant/PeasantMap.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using BlueGOAP;
+using System;
+
+namespace Game.AI
+{
+    public class PeasantMap : MapsBase<ActionEnum, GoalEnum>
+    {
+        public PeasantMap(IAgent<ActionEnum, GoalEnum> agent, Action<IAgent<ActionEnum, GoalEnum>, IMaps<ActionEnum, GoalEnum>> onInitGameData) : base(agent, onInitGameData)
+        {
+
+        }
+
+        protected override void InitActinMaps()
+        {
+            //不可叠加动作状态
+            AddAction<IdleHandler, IdleAction>();
+            AddAction<IdleSwordHandler, IdleSwordAction>();
+            AddAction<AttackHandler, AttackAction>();
+            AddAction<InjureHandler, InjureAction>();
+            AddAction<DeadHandler, DeadAction>();
+            AddAction<MoveHandler, MoveAction>();
+            AddAction<MoveBackwardHandler, MoveBackwardAction>();
+
+            //可叠加动作状态
+            AddAction<AlertHandler, AlertAction>();
+        }
+
+        protected override void InitGoalMaps()
+        {
+            AddGoal<AttackGoal>();
+            AddGoal<IdleSwordGoal>();
+            AddGoal<DeadGoal>();
+            AddGoal<InjureGoal>();
+        }
+
+        protected override void InitGameData(Action<IAgent<ActionEnum, GoalEnum>, IMaps<ActionEnum, GoalEnum>> onInitGameData)
+        {
+            base.InitGameData(onInitGameData);
+        }
+    }
+}
