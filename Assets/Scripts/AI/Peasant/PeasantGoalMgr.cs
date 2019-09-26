c44731f8da839d3f32ee8409c4fa7fbd308e1d0b
李瑞瑞 <18335929360@163.com>
2019-09-26 11:58:23 +0800

    [李瑞瑞]完成Peasant的目标管理器初始化目标对象逻辑

---
+++ Assets/Scripts/AI/Peasant/PeasantGoalMgr.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using BlueGOAP;
+
+namespace Game.AI
+{
+    public class PeasantGoalMgr : GoalManagerBase<ActionEnum, GoalEnum>
+    {
+        public PeasantGoalMgr(IAgent<ActionEnum, GoalEnum> agent) : base(agent)
+        {
+
+        }
+
+        protected override void InitGoals()
+        {
+            AddGoal(GoalEnum.ATTACK);
+            AddGoal(GoalEnum.DEAD);
+            AddGoal(GoalEnum.IDLE_SWORD);
+            AddGoal(GoalEnum.INJURE);
+        }
+    }
+}
