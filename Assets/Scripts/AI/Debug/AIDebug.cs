83deb349f1281a4e54a65663ae55c46f3d667480
李瑞瑞 <18335929360@163.com>
2019-10-08 17:03:52 +0800

    [李瑞瑞]添加是否打印Log的开关

--- Assets/Scripts/AI/Debug/AIDebug.cs
+++ Assets/Scripts/AI/Debug/AIDebug.cs
@@ -5,18 +5,26 @@ namespace Game.AI
 {
     public class AIDebug : DebugMsgBase
     {
+        private bool _canDebug;
+
         public override void Log(string msg)
         {
+            if (!_canDebug)
+                return;
             Debug.Log(msg);
         }
 
         public override void LogError(string msg)
         {
+            if (!_canDebug)
+                return;
             Debug.LogError(msg);
         }
 
         public override void LogWarning(string msg)
         {
+            if (!_canDebug)
+                return;
             Debug.LogWarning(msg);
         }
     }
