4303fb615d62497f5e168fb20e879e599b0e82a8
李瑞瑞 <18335929360@163.com>
2019-10-09 10:50:48 +0800

    [李瑞瑞]View部分的基础逻辑

--- Assets/Scripts/AI/Module/AIViewEffectMgr.cs
+++ Assets/Scripts/AI/Module/AIViewEffectMgr.cs
@@ -1,12 +1,36 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using BlueGOAP;
+using UnityEngine;
 
 namespace Game.AI.ViewEffect
 {
     public class AIViewEffectMgr<T>   
     {
+        private IFSM<T> _fsm;
+        private IFSM<T> _mutilFsm;
+        private Dictionary<T, IActionHandler<T>> _viewDic;
+        private Dictionary<T, IActionHandler<T>> _mutilActionViews;
+
+        public AIViewEffectMgr()
+        {
+            _fsm = new ActionFSM<T>();
+            _mutilFsm = new ActionStateFSM<T>();
+        }
+
         public void ExecuteState(T key)
         {
-
+            if (_viewDic.ContainsKey(key))
+            {
+                _fsm.ExcuteNewState(key);
+            }
+            else if (_mutilActionViews.ContainsKey(key))
+            {
+                _mutilFsm.ExcuteNewState(key);
+            }
+            else
+            {
+                DebugMsg.LogError("动作" + key + "不在当前动作缓存中");
+            }
         }
     }
 }
---
+++ Assets/Scripts/AI/Module/View/AttackView.cs
@@ -0,0 +1,27 @@
+﻿using BlueGOAP;
+using UnityEngine;
+
+namespace Game.AI.ViewEffect
+{
+    public class AttackView<T> : IFsmState<T>
+    {
+        public ActionExcuteState ExcuteState { get; }
+
+        public T Label { get; }
+
+        public void Enter()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Execute()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Exit()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
